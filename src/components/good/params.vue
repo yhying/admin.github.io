6b53156711e65866bbc9d20e568f343666210377
18627805032@163.com <18627805032@163.com>
2020-01-31 20:22:00 +0800

    商品参数

--- src/components/good/params.vue
+++ src/components/good/params.vue
@@ -6,7 +6,7 @@
             <el-breadcrumb-item>商品参数</el-breadcrumb-item>
         </el-breadcrumb>
         <el-card>
-            <el-alert title="警告提示的文案" type="warning" :closable="false" show-icon>
+            <el-alert title="注意：只允许修改三级分类的参数" type="warning" :closable="false" show-icon>
             </el-alert>
             <el-row>
                 <el-col>
@@ -15,6 +15,32 @@
                         @change="handleChange" clearable></el-cascader>
                 </el-col>
             </el-row>
+            <el-tabs v-model="activeName" @tab-click="handleClick">
+                <el-tab-pane label="动态参数" name="many">
+                    <el-button type="primary" size="mini" :disabled="isdisabled">添加参数</el-button>
+                    <el-table :data="manyTableData" border stripe max-height="490px">
+                        <el-table-column type="expand">
+                        </el-table-column>
+                        <el-table-column type="index" label="#">
+                        </el-table-column>
+                        <el-table-column prop="attr_name" label="参数名称">
+                        </el-table-column>
+                        <el-table-column label="操作">
+                            <template slot-scope="scope">
+                                <el-button type="primary" icon="el-icon-edit" size="mini"
+                                    @click="showEditDailog(scope.row)">编辑
+                                </el-button>
+                                <el-button type="danger" icon="el-icon-delete" size="mini"
+                                    @click="deleteUser(scope.row.id)">删除
+                                </el-button>
+                            </template>
+                        </el-table-column>
+                    </el-table>
+                </el-tab-pane>
+                <el-tab-pane label="静态属性" name="only">
+                    <el-button type="primary" size="mini" :disabled="isdisabled">添加属性</el-button>
+                </el-tab-pane>
+            </el-tabs>
         </el-card>
     </div>
 </template>
@@ -28,12 +54,28 @@
                     value: 'cat_id',
                     label: 'cat_name',
                     children: 'children',
-                }
+                },
+                activeName: 'first',
+                manyTableData: [],
+                onlyTableData: [],
+                onList: [],
+                activeName: 'many'
 
             }
         },
         created() {
             this.getcatesList()
+            // console.log(this.selectKeys);
+        },
+        computed: {
+            // 监听按钮禁用
+            isdisabled() {
+                if (this.selectKeys.length === 3) {
+                    return false;
+                } else {
+                    return true
+                }
+            }
         },
         methods: {
             getcatesList() {
@@ -49,11 +91,40 @@
                 })
             },
             handleChange() {
-                console.log(this.selectKeys);
-                if(this.selectKeys.length<3){
-                   this.selectKeys=[]
-                   return;
+                // console.log(this.selectKeys[this.selectKeys.length-1]);
+                if (this.selectKeys.length < 3) {
+                    this.selectKeys = []
+                    return;
                 }
+                this.getgoodprams()
+            },
+            handleClick() {
+                console.log(this.activeName);
+                this.getgoodprams()
+            },
+            getgoodprams() {
+                this.$http.get('categories/' + this.selectKeys[this.selectKeys.length - 1], {
+                    params: {
+                        sel: this.activeName
+                    }
+                }).then(res => {
+                    console.log(res);
+                    if (res.data.meta.status == 200) {
+                        if(this.activeName==='many'){
+                            this.manyTableData = res.data.data
+                        }else {
+                            this.onlyTableData = res.data.data
+                        }
+                        console.log(this.manyTableData);
+                        console.log(this.onlyTableData);
+                    } else {
+                        this.$message({
+                            type: 'error',
+                            message: '获取参数列表失败',
+                            duration: 1000
+                        });
+                    }
+                })
             }
         },
     }
@@ -61,9 +132,14 @@
 <style lang="less" scoped>
     .el-row {
         margin-top: 15px;
+        margin-bottom: 10px;
     }
 
     .goods {
         margin-right: 15px;
+    }
+
+    .el-cascader {
+        width: 30%;
     }
 </style>
