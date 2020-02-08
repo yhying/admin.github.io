2b00ed0fcd8ee9c75a0aaac0f2f4a3095e865a4a
18627805032@163.com <18627805032@163.com>
2020-02-08 21:45:11 +0800

    添加商品

--- src/components/good/add.vue
+++ src/components/good/add.vue
@@ -18,7 +18,8 @@
             </el-steps>
             <el-form :model="addgoodsForm" :rules="addgoodsrules" ref="ruleForm" label-width="100px"
                 label-position="top">
-                <el-tabs v-model="activeIndex" :tab-position="tabPosition">
+                <el-tabs v-model="activeIndex" :tab-position="tabPosition" :before-leave="beforeTabLeave"
+                    @tab-click="tabClicked">
                     <el-tab-pane label="基本信息" name="0">
                         <el-form-item label="商品名称" prop="goods_name">
                             <el-input v-model="addgoodsForm.goods_name"></el-input>
@@ -59,7 +60,12 @@
                     goods_price: '',
                     goods_weight: '',
                     goods_number: '',
-                    goods_cat:[]
+                    goods_cat: [],
+                    // 图片的数组
+                    pics: [],
+                    // 商品的详情描述
+                    goods_introduce: '',
+                    attrs: []
                 },
                 addgoodsrules: {
                     goods_name: [{
@@ -92,7 +98,13 @@
                     label: 'cat_name',
                     value: 'cat_id',
                     children: 'children'
-                }
+                },
+                // 动态参数列表数据
+                manyTableData: [],
+                // 静态属性列表数据
+                onlyTableData: [],
+                // 上传图片的URL地址
+                uploadURL: 'http://127.0.0.1:8888/api/private/v1/upload',
             }
         },
         created() {
@@ -109,9 +121,66 @@
                 })
             },
             handleChange(e) {
-                console.log(e);
-            }
+                // console.log(e);
+                if (this.addgoodsForm.goods_cat.length !== 3) {
+                    this.addgoodsForm.goods_cat = []
+                }
+            },
+            beforeTabLeave(activeName, oldActiveName) {
+                // console.log('即将离开的标签页名字是：' + oldActiveName)
+                // console.log('即将进入的标签页名字是：' + activeName)
+                // return false
+                if (oldActiveName === '0' && this.addgoodsForm.goods_cat.length !== 3) {
+                    this.$message.error('请先选择商品分类！')
+                    return false
+                }
+            },
+            tabClicked() {
+                // console.log(this.activeIndex)
+                // 证明访问的是动态参数面板
+                if (this.activeIndex === '1') {
+                    this.$http.get(
+                        `categories/${this.cateId}/attributes`, {
+                            params: {
+                                sel: 'many'
+                            }
+                        }
+                    ).then(res => {
+                        if (res.data.meta.status !== 200) {
+                            return this.$message.error('获取动态参数列表失败！')
+                        }
+                        console.log(res.data.data)
+                        res.data.data.forEach(item => {
+                            item.attr_vals =
+                                item.attr_vals.length === 0 ? [] : item.attr_vals.split(' ')
+                        })
+                        this.manyTableData = res.data.data
+                    })
+                } else if (this.activeIndex === '2') {
+                    this.$http.get(
+                        `categories/${this.cateId}/attributes`, {
+                            params: {
+                                sel: 'only'
+                            }
+                        }
+                    ).then(res => {
+                        if (res.data.meta.status !== 200) {
+                            return this.$message.error('获取静态属性失败！')
+                        }
+                        console.log(res.data.data)
+                        this.onlyTableData = res.data.data
+                    })
+                }
+            },
         },
+        computed: {
+            cateId() {
+                if (this.addgoodsForm.goods_cat.length === 3) {
+                    return this.addgoodsForm.goods_cat[2]
+                }
+                return null
+            }
+        }
     }
 </script>
 <style lang="less">
